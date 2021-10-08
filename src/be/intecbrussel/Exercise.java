301475849a4afac9cb61d51dfdd47e9007481f51
JohnDough13 <jan.blommaert@student.intecbrussel.be>
2021-10-08 09:48:56 +0200

    Initial commit

---
+++ src/be/intecbrussel/Exercise.java
@@ -0,0 +1,88 @@
+package be.intecbrussel;
+
+public class Exercise {
+    public void excerciseOne() {
+
+        int a = 5;
+        int b = 7;
+        int c = 4;
+
+        if (a > b) {
+            if (a > c) {
+                System.out.println(a);
+            }
+        } else if (b > a) {
+            if (b > c) {
+                System.out.println(b);
+            }
+        } else {
+            System.out.println(c);
+        }
+    }
+
+    public void secondExcercise() {
+
+        int numberToTest = -9;
+
+        if (numberToTest > 0) {
+            System.out.println("Number is positive");
+
+        } else if (numberToTest < 0) {
+            System.out.println("Number is negative");
+        } else {
+            System.out.println("Number is 0");
+        }
+    }
+
+    public void thirdExcercise() {
+        int dayOfTheWeek = 9;
+
+        if (dayOfTheWeek == 1) {
+            System.out.println("Monday");
+        } else if (dayOfTheWeek == 2) {
+            System.out.println("Tuesday");
+        } else if (dayOfTheWeek == 3) {
+            System.out.println("Wednesday");
+        } else if (dayOfTheWeek == 4) {
+            System.out.println("Thursday");
+        } else if (dayOfTheWeek == 5) {
+            System.out.println("Friday");
+        } else if (dayOfTheWeek == 6) {
+            System.out.println("Saturday");
+        } else if (dayOfTheWeek == 7) {
+            System.out.println("Sunday");
+        } else {
+            System.out.println("Day is invalid");
+        }
+    }
+
+    public void fourthExcercise() {
+        //4. Print alle getallen van 1 tot en met 10.
+        for (int a = 0; a <= 10; a++) {
+            System.out.println(a);
+        }
+    }
+    public void fifthExcercise() {
+        //5. Vraag een getal aan de gebruiker. Print alle veelvouden van dat getal tot 10.
+        int a = 5;
+        int b = 2;
+        int boolean numberEntry = a;
+        if numberEntry == true;
+        System.out.println(Multiplier)
+            Multiplier (a,b);{
+                d=a*b
+                return d
+                for (int c = 1; c<=10; c++)
+                 Result a=a*c
+
+                Print result
+
+            }
+        }
+
+
+    }
+}
+
+
+
---
+++ src/be/intecbrussel/HelloWorld.java
@@ -0,0 +1,18 @@
+package be.intecbrussel;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+
+        Exercise myExercises = new Exercise();
+
+        myExercises.excerciseOne();
+        myExercises.secondExcercise();
+        myExercises.thirdExcercise();
+        myExercises.fourthExcercise();
+        myExercises.fifthExcercise();
+    }
+
+}
+
+
+
